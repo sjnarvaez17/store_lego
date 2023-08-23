7fad9b10be1e1755d9b957282b1c9adb3c971ddb
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2023-08-22 21:30:16 -0500

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -36,6 +37,19 @@ android {
 }
 
 dependencies {
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation ("com.google.dagger:dagger:2.44.2")
+    kapt ("com.google.dagger:dagger-compiler:2.44.2")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+
+    implementation ("androidx.core:core-splashscreen:1.0.0")
+
+    implementation ("androidx.room:room-runtime:2.4.3")
+    annotationProcessor ("androidx.room:room-compiler:2.4.3")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
