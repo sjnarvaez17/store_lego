2f96b6d6b95a6b65c1c1a5104706e6d63e0b3328
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2023-08-23 03:03:40 -0500

    add retrofit to dependency injection

---
+++ app/src/main/java/com/example/storelego/core/di/ApplicationModule.kt
@@ -0,0 +1,18 @@
+package com.example.storelego.core.di
+
+import com.example.storelego.AndroidApplication
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+class ApplicationModule(private val application: AndroidApplication) {
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl("https://us-central1-api-back-admission-test.cloudfunctions.net/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
--- app/src/main/java/com/example/storelego/domain/repository/GetProductRepo.kt
+++ app/src/main/java/com/example/storelego/domain/repository/GetProductRepo.kt
@@ -2,7 +2,6 @@ package com.example.storelego.domain.repository
 
 import com.example.storelego.core.functional.Response
 import com.example.storelego.domain.model.Product
-import javax.inject.Singleton
 
 interface GetProductRepo {
 
